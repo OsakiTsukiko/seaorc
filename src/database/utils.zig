e936cb51e38045ad1c649f36eb8dae8299749d13
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2025-01-12 21:01:19 +0200

    add login functionality

--- src/database/utils.zig
+++ src/database/utils.zig
@@ -9,4 +9,14 @@ pub const DBUtils = struct {
         const user_id = conn.lastInsertedRowId();
         return user_id;
     }
+
+    pub fn getUser(conn: *zqlite.Conn, username: []const u8, passwordHash: []const u8) !i64 {
+        const row = try conn.row("select * from users where username = ?1 and passwordhash = ?2 limit 1", .{username, passwordHash});
+        if (row) |user| {
+            defer user.deinit();
+            return user.int(0);
+        } else {
+            return error.UserNotInDatabase;
+        }
+    }
 };
--- src/handler/login.zig
+++ src/handler/login.zig
@@ -4,6 +4,7 @@ const json = std.json;
 const httpz = @import("httpz");
 
 const Global = @import("../domain/global.zig").Global;
+const DBUtils = @import("../database/utils.zig").DBUtils;
 
 const LoginBody = struct {
     username: []const u8,
@@ -24,7 +25,7 @@ pub fn login(global: *Global, req: *httpz.Request, res: *httpz.Response) !void {
             // error.SyntaxError
             // error.UnknownField
             // error.MissingField
-            
+
             res.status = 400; // BAD REQUEST
             try res.json(.{
                 .err = "Bad request body!",
@@ -32,8 +33,33 @@ pub fn login(global: *Global, req: *httpz.Request, res: *httpz.Response) !void {
             return;
         };
         defer parsed_body.deinit();
-
-        std.debug.print("USER: {s} {s}\n", .{parsed_body.value.username, parsed_body.value.password});
+        
+        // TODO: HASH PASSWORD!!!!
+        const user_id = DBUtils.getUser(global.dbconn, parsed_body.value.username, parsed_body.value.password) catch |err| {
+            switch (err) {
+                error.UserNotInDatabase => {
+                    res.status = 401; // UNAUTHORIZED
+                    try res.json(.{
+                        .err = "WRONG USERNAME or PASSWORD!",
+                    }, .{});
+                    return;
+                },
+                else => {
+                    log.debug("⚠️  DATABASE ERROR on LOGIN: {s} {any}", .{parsed_body.value.username, err});
+                    res.status = 500; // INTERNAL SERVER ERROR
+                    try res.json(.{
+                        .err = "Some database error OCCURED!",
+                    }, .{});
+                    return;
+                }
+            }
+        };
+        
+        res.status = 200; // OK
+        try res.json(.{
+            .user_id = user_id,
+        }, .{});
+        return;
     } else { // no body
         res.status = 400; // BAD REQUEST
         try res.json(.{
